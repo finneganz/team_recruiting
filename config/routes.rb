5dbc7e17543fe94960575c389bf8dda71d1dc714
finneganz <abs0lutely.n0nsense.cs@gmail.com>
2019-10-07 20:17:52 +0900

    Add _header.html and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #devise_for :users
   root 'users#index'
+  get '/help' => 'users#help', as: :help
   #get '/show/:id' => 'users#show', as: :users_show
   #get '/signup' => 'users#new',as: :signup
   #post '/signup' => 'users#create'
