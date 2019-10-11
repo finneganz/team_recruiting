af197a165fdfcdc18758a71aa2c9212cf3429ef9
finneganz <abs0lutely.n0nsense.cs@gmail.com>
2019-10-11 23:56:13 +0900

    Add scout page

--- app/controllers/scouts_controller.rb
+++ app/controllers/scouts_controller.rb
@@ -3,8 +3,16 @@ class ScoutsController < ApplicationController
   end
 
   def index
+    @scouts = Scout.where(scout_flg: 1)
+    @print = @scouts
+    if @scouts.blank?
+      @scouts = nil
+    else
+    end
   end
 
   def show
+      @scout = Scout.find(params[:id])
   end
+
 end
